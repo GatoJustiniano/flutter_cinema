c147a5937604b346d3c08740db59a8b208db5fb0
GatoJustiniano <andres95liverpool@gmail.com>
2024-08-08 00:43:29 -0400

    implements mapper in datasource

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter_app_cinema/config/constant/environment.dart';
 import 'package:flutter_app_cinema/domain/datasources/movies_datasource.dart';
 import 'package:flutter_app_cinema/domain/entities/movie.dart';
+import 'package:flutter_app_cinema/infrastructure/mappers/movie_mapper.dart';
+import 'package:flutter_app_cinema/infrastructure/models/moviedb/moviedb_response.dart';
 
 class MovieDBDataSource extends MoviesDatasource {
   final dio = Dio(
@@ -16,8 +18,12 @@ class MovieDBDataSource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('movie/now_playing');
-    final List<Movie> list_Movies = [];
+    final respuesta = MovieDbResponse.fromJson(response.data);
+    final List<Movie> listMovies = respuesta.results
+        .where((element) => element.posterPath != 'no-poster')
+        .map((item) => MovieMapper.movieDBToEntity(item))
+        .toList();
 
-    return [];
+    return listMovies;
   }
 }
